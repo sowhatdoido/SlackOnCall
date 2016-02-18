8813322835ba7111577e5bdc4f1d1825c392bcf8
Frank Tran <frank@freshiscool.com>
2016-02-18 00:50:47 -0800

    Initial design for idle status page

---
+++ public/main.js
@@ -0,0 +1,31 @@
+var randomNumber = function (min, max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+var setCallState = function(state){
+    if(state){
+        $('body').addClass('active-call');
+    } else {
+        $('body').removeClass('active-call');
+    }
+}
+
+var rerollBubble = function($element){
+    var $this = $(this);
+    if(typeof $element != 'number')
+        $this = $element;
+        
+    var bubbleSize = randomNumber(15, 200) + 'px';
+    $this.css({
+        left: randomNumber(-15, 100) + '%',
+        width: bubbleSize,
+        height: bubbleSize,
+        opacity: randomNumber(10, 50) * 0.01,
+        'animation-delay': randomNumber(1, 5) + 's',
+        'animation-duration': randomNumber(10, 20) + 's'
+    });
+};
+
+$('.bubble')
+    .each(rerollBubble);
