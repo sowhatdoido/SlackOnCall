1f6012a3bea1a145d9d67a468571cff3447eb585
Frank Tran <frank@freshiscool.com>
2016-02-17 21:59:29 -0800

    Set up dotenv and endpoint for slack command

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+require('dotenv').config();
+
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 5000;
 var io = require('socket.io')(app.listen(port));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/public'));
+app.post('/endpoint', function (req, res) {
+    var token = req.body.token;
+
+    if(token !== process.env.SLACK_TOKEN){
+        res.json({
+            error: 'Token Mismatch'
+        });
+        return;
+    }
+    
+    res.json(req.body);
+});
 
 io.on('connection', function(socket){
    console.log('Socket Connected'); 
